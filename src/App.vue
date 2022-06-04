3e44192b1956aa5e480d2078a64f8dd4a4fc20b4
josty <josty.qweenq@gmail.com>
2022-06-04 18:28:41 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <script setup>
-import Avatar from "./components/Avatar.vue";
-import Contact from "./components/Contact.vue";
-import GithubLogo from '../public/img/logo/github.svg'
-import SpotifyLogo from '../public/img/logo/spotify.svg'
-import VkLogo from '../public/img/logo/vk.svg'
-import DiscordLogo from '../public/img/logo/discord.svg'
-import TwitterLogo from '../public/img/logo/twitter.svg'
-import TwitchLogo from '../public/img/logo/twitch.svg'
-import OsuLogo from '../public/img/logo/osu.svg'
-import ShikimoriLogo from '../public/img/logo/shikimori.svg'
-import EmailLogo from '../public/img/logo/email.svg'
-
+import GithubLogo from "/public/img/logo/Github.svg";
+import SpotifyLogo from "/public/img/logo/Spotify.svg";
+import VkLogo from "/public/img/logo/VK.svg";
+import TwitterLogo from "/public/img/logo/Twitter.svg";
+import TwitchLogo from "/public/img/logo/Twitch.svg";
+import OsuLogo from "/public/img/logo/osu.svg";
+import ShikimoriLogo from "/public/img/logo/Shikimori.svg";
+import EmailLogo from "/public/img/logo/Email.svg";
 
 const contacts = [
   {
-    title: "Github",
-    logo: GithubLogo,
-    url: "https://github.com/JQweenq",
+    title: "Вконтакте",
+    logo: VkLogo,
+    url: "https://vk.com/jqweenq",
   },
   {
-    title: "VK",
-    logo: VkLogo,
-    url: "https://vk.com/JQweenq",
+    title: "Электропочка",
+    logo: EmailLogo,
+    url: "mailto:jqweenq@gmail.com",
   },
+];
+const otherContacts = [
   {
     title: "Shikimori",
     logo: ShikimoriLogo,
@@ -34,68 +32,154 @@ const contacts = [
     url: "https://osu.ppy.sh/users/16883323",
   },
   {
-    title: "Spotify",
-    logo: SpotifyLogo,
-    url: "https://github.com/JQweenq",
-  },
-  {
-    title: "Twitch",
-    logo: TwitchLogo,
+    title: "Github",
+    logo: GithubLogo,
     url: "https://github.com/JQweenq",
   },
   {
     title: "Twitter",
     logo: TwitterLogo,
-    url: "https://github.com/JQweenq",
+    url: "https://twitter.com/JQweenq",
   },
   {
-    title: "Discord",
-    logo: DiscordLogo,
-    url: "https://github.com/JQweenq",
+    title: "Twitch",
+    logo: TwitchLogo,
+    url: "https://twitch.tv/josty_qweenq",
   },
   {
-    title: "Email",
-    logo: EmailLogo,
-    url: "mailto:jqweenq@gmail.com",
+    title: "Spotify",
+    logo: SpotifyLogo,
+    url: "https://github.com/jqweenq",
   },
 ];
-</script>
 
+function open(link) {
+  window.open(link, "_blank");
+}
+</script>
 <template>
-  <Avatar title="Josty Qweenq" url="img/avatar.png" />
-  <div class="contacts-wrapper">
-    <Contact
-      v-for="contact in contacts"
-      :key="contact.title"
-      :title="contact.title"
-      :logo="contact.logo"
-      :url="contact.url"
-    />
+  <div class="part hello-part">
+    <h2 class="part_title">Добро пожаловать</h2>
+    <p>
+      Я — программист. Основной специальности нету, но иногда занимаюсь
+      мобильной разработкой, а иногда веб-разработкой, а бывает такое что пишу
+      скрипты или изучаю для себя новою сферу по типу администрирования систем
+      на базе ядра Linux.
+    </p>
   </div>
-  <h2>My projects</h2>
-  <div class="">
-	  <p>Genshin Journey APK</p>
-	  <span>Мобильное приложение для <a href="https://genshin-journey.ml" target="_blank">сайта</a> написанное под Android на языке программирования Java, но в будующем планируется перейти на Kotlin, в нем используются такие библиотеки как Retrofit, OkHttpClient и LoggingInterceptor для него, сериализатор Gson</span>
+  <div class="part part-projects">
+    <h2 class="part_title">Мои проекты</h2>
+    <img class="img-genshin" src="/img/Genshin.png" alt="Genshin" />
+    <p>
+      Приложение для моего знакомого (не знакомого) сделаное чисто по рофлу.
+    </p>
+    <p>
+      Сам сайт и приложение написаны по тематике Genshin Impact, в приложении
+      есть (или будут) реализации того, что есть на сайте. На данный момент в
+      приложении есть список персонажей, список молитв и словарь.
+    </p>
+    <p>
+      Написано на Java, есть подгрузка данных с бекэнда и в процессе создания
+      отправка данных (планируется редактор записей). В скором времени будет
+      переписываться на Kotlin
+    </p>
+    <div class="button" @click="open('https://github.com/jqweenq/Genshin')">
+      <div v-html="GithubLogo"></div>
+      <p class="button__text">Github</p>
+    </div>
   </div>
-  <div class="">
-	  <p>Genshin Journey Backend</p>
-	  <span>Backend для <a href="https://genshin-journey.ml" target="_blank">сайта</a> написанный на языке программирования Python с использованием микрофреймворка Flask и многих дополнений к нему. В данный момент не используется, так как не стабильно и полностью не дописанно, но тестовый релиз можно найти на субдомене <a href="http://api.josty.ml">api.josty.ml</a>, из-за того что хостингом является бесплатный сервис Heroku, спустя некоторое время сервис отключает контейнер и при подключении нового клиета его ожидает 10 секундный тайм-аут</span>
+  <div class="part part-contacts">
+    <h2 class="part_title">Мои контакты</h2>
+    <div class="part-buttons-wrapper">
+      <div
+        class="button"
+        v-for="contact in contacts"
+        :key="contact.title"
+        @click="open(contact.url)"
+      >
+        <div v-html="contact.logo"></div>
+        <p class="button__text">{{ contact.title }}</p>
+      </div>
+    </div>
+  </div>
+  <div class="part">
+    <h2 class="part_title">Где ещё вы можете меня встретить</h2>
+    <div class="part-buttons-wrapper">
+      <div
+        class="button"
+        v-for="contact in otherContacts"
+        :key="contact.title"
+        @click="open(contact.url)"
+      >
+        <div v-html="contact.logo"></div>
+        <p class="button__text">{{ contact.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
 body {
-  background: #070720 url("/img/pattern.png") 0 0/400px 300px repeat;
+  background: #111;
   min-height: 100%;
+  font-family: Inter-Regular;
+}
+h2 {
+  margin: 0 0 25px 0;
 }
 .wrapper {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #a8a8a8;
-}
-.contacts-wrapper {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 0 100px 0;
+  min-height: 100%;
+}
+.part {
+  margin: 0 0 50px 0;
+  color: #fff;
+  font-size: 22px;
+  width: 100%;
+}
+.part_title {
+  font-family: Inter-Bold;
+  font-size: 32px;
+}
+.img-genshin {
+  width: 270px;
+  border-radius: 16px;
+  margin: 0 25px 25px 0;
+}
+.part-buttons-wrapper {
+  display: grid;
   justify-content: space-evenly;
+  grid-template-columns: 1fr 1fr;
+}
+.button {
+  display: flex;
+  align-items: center;
+  padding: 32px;
+  color: #fff;
+  border: 1px solid #474747;
+  border-radius: 16px;
+  transition: 0.1s;
+  text-decoration: none;
+  font-family: Inter-SemiBold;
+  margin: 10px 10px;
+}
+.button:hover {
+  color: #fff;
+  border: 1px solid #fff;
+}
+.button svg {
+  flex-basis: 32px;
+}
+.button__text {
+  height: 100%;
+  line-height: 32px;
+  flex-basis: 100%;
+  margin: 0 0 0 32px;
 }
 </style>
