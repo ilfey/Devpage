53968f1718b2e4e74644b76749dc456f00b7ca04
JQweenq <josty.qweenq@gmail.com>
2022-08-30 13:17:49 +0700

    add new color-scheme and theme switcher

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import TwitchLogo from "/public/img/logo/Twitch.svg";
 import OsuLogo from "/public/img/logo/osu.svg";
 import ShikimoriLogo from "/public/img/logo/Shikimori.svg";
 import EmailLogo from "/public/img/logo/Email.svg";
+import Navbar from "./components/Navbar.vue";
 
 const contacts = [
   {
@@ -28,6 +29,7 @@ const contacts = [
     url: "mailto:jqweenq@gmail.com",
   },
 ];
+
 const otherContacts = [
   {
     title: "AniList",
@@ -75,122 +77,139 @@ function open(link) {
   window.open(link, "_blank");
 }
 </script>
+
 <template>
-  <div class="part hello-part">
-    <h2 class="part_title">Добро пожаловать</h2>
-    <p>
-      Я — программист. Основной специальности нету, но иногда занимаюсь
-      мобильной разработкой, а иногда веб-разработкой, а бывает такое, что пишу
-      скрипты или изучаю для себя новою сферу, по типу администрирования систем
-      на базе ядра Linux.
-    </p>
-  </div>
-  <div class="part part-projects">
-    <h2 class="part_title">Мои проекты</h2>
-    <img class="img-genshin" src="/img/Genshin.png" alt="Genshin" />
-    <p>
-      Приложение для моего знакомого (не знакомого) сделанное чисто по рофлу.
-    </p>
-    <p>
-      Сам сайт и приложение написаны по тематике Genshin Impact, в приложении
-      есть (или будут) реализации того, что есть на сайте. На данный момент в
-      приложении есть список персонажей, список молитв и словарь.
-    </p>
-    <p>
-      Написано на Java, есть подгрузка данных с бекэнда и в процессе создания
-      отправка данных (планируется редактор записей). В скором времени будет
-      переписываться на Kotlin, потому что написание кода происходит быстрее и
-      проще, чем на Java.
-    </p>
-    <div class="button" @click="open('https://github.com/jqweenq/Genshin')">
-      <div v-html="GithubLogo"></div>
-      <p class="button__text">Github</p>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <div class=" part hello-part">
+      <h2 class="part__title">Добро пожаловать</h2>
+      <p>
+        Я — программист. Основной специальности нету, но иногда занимаюсь
+        мобильной разработкой, а иногда веб-разработкой, а бывает такое, что пишу
+        скрипты или изучаю для себя новою сферу, по типу администрирования систем
+        на базе ядра Linux.
+      </p>
     </div>
-  </div>
-  <div class="part part-contacts">
-    <h2 class="part_title">Мои контакты</h2>
-    <div class="buttons-wrapper">
-      <div
-        class="button"
-        v-for="contact in contacts"
-        :key="contact.title"
-        @click="open(contact.url)"
-      >
-        <div v-html="contact.logo"></div>
-        <p class="button__text">{{ contact.title }}</p>
+    <div class="part part-projects">
+      <h2 class="part__title">Мои проекты</h2>
+      <img class="img-genshin" src="/img/Genshin.png" alt="Genshin" />
+      <p>
+        Приложение для моего знакомого (не знакомого) сделанное чисто по рофлу.
+      </p>
+      <p>
+        Сам сайт и приложение написаны по тематике Genshin Impact, в приложении
+        есть (или будут) реализации того, что есть на сайте. На данный момент в
+        приложении есть список персонажей, список молитв и словарь.
+      </p>
+      <p>
+        Написано на Java, есть подгрузка данных с бекэнда и в процессе создания
+        отправка данных (планируется редактор записей). В скором времени будет
+        переписываться на Kotlin, потому что написание кода происходит быстрее и
+        проще, чем на Java.
+      </p>
+      <div class="button" @click="open('https://github.com/jqweenq/Genshin')">
+        <div v-html="GithubLogo"></div>
+        <p class="button__text">Github</p>
       </div>
     </div>
-  </div>
-  <div class="part">
-    <h2 class="part_title">Где ещё вы можете меня встретить</h2>
-    <div class="buttons-wrapper">
-      <div
-        class="button"
-        v-for="contact in otherContacts"
-        :key="contact.title"
-        @click="open(contact.url)"
-      >
-        <div v-html="contact.logo"></div>
-        <p class="button__text">{{ contact.title }}</p>
+    <div class="part part-contacts">
+      <h2 class="part__title">Мои контакты</h2>
+      <div class="buttons-wrapper">
+        <div class="button" v-for="contact in contacts" :key="contact.title" @click="open(contact.url)">
+          <div v-html="contact.logo"></div>
+          <p class="button__text">{{ contact.title }}</p>
+        </div>
       </div>
     </div>
-  </div>
+    <div class="part">
+      <h2 class="part__title">Где ещё вы можете меня встретить</h2>
+      <div class="buttons-wrapper">
+        <div class="button" v-for="contact in otherContacts" :key="contact.title" @click="open(contact.url)">
+          <!-- FIXME XSS -->
+          <div v-html="contact.logo" />
+          <p class="button__text">{{ contact.title }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
-body {
-  background: #111;
-  min-height: 100%;
-  font-family: Inter-Regular;
+header {
+  margin: 0 auto;
 }
-h2 {
-  margin: 0 0 25px 0;
-}
-.wrapper {
+
+main {
   position: absolute;
   left: 50%;
   transform: translate(-50%);
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 100px 0 100px 0;
-  min-height: 100%;
+  margin: 25px 0 100px 0;
 }
+
+h2 {
+  margin: 0 0 25px 0;
+}
+
 .part {
-  margin: 0 0 50px 0;
-  color: #fff;
+  margin: 0 0 25px 0;
+  color: var(--on-background);
   font-size: 22px;
   width: 100%;
 }
-.part_title {
+
+.part__title {
   font-family: Inter-Bold;
   font-size: 32px;
 }
+
+.part>p {
+  margin: 22px 0;
+}
+
 .img-genshin {
   width: 270px;
   border-radius: 16px;
   margin: 0 25px 25px 0;
 }
+
 .buttons-wrapper {
   display: grid;
   justify-content: space-evenly;
 }
+
 .button {
   display: flex;
   align-items: center;
   padding: 32px;
-  color: #fff;
-  border: 1px solid #474747;
+  color: var(--primary);
+  border: 1px solid var(--primary);
   border-radius: 16px;
   transition: 0.1s;
-  text-decoration: none;
   font-family: Inter-SemiBold;
   margin: 10px 10px;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
+
+.button svg path {
+  fill: var(--primary);
+}
+
 .button:hover {
-  color: #fff;
-  border: 1px solid #fff;
+  color: var(--secondary);
+  border: 1px solid var(--secondary);
 }
+
+.button:hover svg path {
+  fill: var(--secondary);
+}
+
 .button__text {
   height: 100%;
   line-height: 32px;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  data: () => ({
+    currentLang: "En",
+    currentScheme: "dark",
+  }),
+  methods: {
+    toggleLang() {
+      alert("You clicked on the language switcher! This will be done soon. x(")
+    },
+    toggleTheme() {
+      const htmlClasses = document.documentElement.classList
+
+      if (htmlClasses.contains("dark")) {
+        this.currentScheme = "light"
+        htmlClasses.replace("dark", this.currentScheme)
+      } else {
+        this.currentScheme = "dark"
+        htmlClasses.replace("light", this.currentScheme)
+      }
+      localStorage.setItem("color-scheme", this.currentScheme)
+    },
+  },
+  beforeMount() {
+    const scheme = localStorage.getItem("color-scheme")
+    if (scheme) {
+      this.currentScheme = scheme
+    }
+  },
+}
+</script>
+
+<template>
+  <nav>
+    <p @click="toggleLang">{{ currentLang == 'Ru' ? 'En' : 'Ru' }}</p>
+    <p @click="toggleTheme">{{ currentScheme == "dark" ? "Light" : "Dark" }}</p>
+  </nav>
+</template>
+
+<style>
+nav {
+  margin: 25px 0 0 0;
+  font-size: 22px;
+  font-family: Inter-SemiBold;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
