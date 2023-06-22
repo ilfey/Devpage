e3c9f7f2388c0dc48e9043360e80891611c26043
ilfey <ilfey.global@gmail.com>
2023-06-22 18:21:50 +0300

    add vite-plugin-html and remove dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react-swc'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin(),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss],
