146c7221e2ffb7a9405101d4a69eb0917bbe1a4a
Oleg <scherbaoleg4@gmail.com>
2023-07-10 19:56:21 +0300

    init test

--- src/features/Message/MessageImage.tsx
+++ src/features/Message/MessageImage.tsx
@@ -5,9 +5,13 @@ interface Props {
 }
 
 export default function MessageImage({ link, classList }: Props) {
+  const handleImageError = () => {
+    console.log('image load error')
+  };
+
   return (
     <div className={`max-h-64 max-w-64 rounded-md flex flex-col overflow-hidden justify-center ${classList}`}>
-      <img src={link} alt="image" />
+      <img src={link} alt="image" onError={handleImageError}/>
     </div>
   )
 }
