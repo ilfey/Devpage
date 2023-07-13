2a71cdac07bd067e559b7f31d22b5a27c415a877
ilfey <ilfey.global@gmail.com>
2023-07-13 20:13:26 +0700

    add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,11 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					{/* TODO: add x32, x256 and x512 */}
+					<link rel='icon' href='/favicon-x32.png' sizes='32x32' />
 					<link rel='icon' href='/favicon-x64.png' sizes='64x64' />
 					<link rel='icon' href='/favicon-x128.png' sizes='128x128' />
+					<link rel='icon' href='/favicon-x256.png' sizes='256x256' />
+					<link rel='icon' href='/favicon-x512.png' sizes='512x512' />
 				</Head>
 				<body className='h-screen'>
 					<Main />
